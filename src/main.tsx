f346935c442e17f2add092d48a5cfef2ea8f82dd
Gabriel-Aco <gabriel.santos@acovisa.com.br>
2022-11-23 16:03:22 -0300

    Alterando exportação do arquivo para o single-spa

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from "react-dom";
+import App from './App';
+// SingleSpaContext is a react@16.3 (if available) context that provides singleSpa props
+import singleSpaReact, { SingleSpaContext } from 'single-spa-react';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-      <App />
-  </React.StrictMode>
-)
+export const { bootstrap, mount, unmount } = singleSpaReact({
+  React,
+  ReactDOM,
+  rootComponent: App,
+  errorBoundary(err, info, props) {
+    // https://reactjs.org/docs/error-boundaries.html
+    return <div>This renders when a catastrophic error occurs</div>;
+  },
+});
